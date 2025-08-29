eca05231ea3a87d1fd3c1b8d3c36ca3bdc8372ff
Zhengbang <zzb_99@hotmail.con>
2025-08-29 18:47:37 +0800

    change, ecut in testfile

--- kssolv2.0_GW_test/test_o2_222_spin_gw.m
+++ kssolv2.0_GW_test/test_o2_222_spin_gw.m
@@ -10,7 +10,7 @@ KSSOLV_startup;
 
 [sys, options, syms, myneed] = read_qe_gw_bgw('.\gw\qe_data\o2_222_spin');
 save('groundstate.mat', 'myneed');
-return;
+% return;
 [sys, options] = gw_setup(sys, options);
 
 eps.nbnd = 15;
@@ -18,14 +18,14 @@ eps.nv = options.nv;
 eps.nc = eps.nbnd - eps.nv;
 omega = 0;
 eta = 0;
-eps.cutoff = 2;
-eps.coul_cutoff = 2; %coulomb truncation radius in epsilon
+eps.cutoff = 200;
+eps.coul_cutoff = 200; %coulomb truncation radius in epsilon
 eps = epsilon(sys, options, syms, eps);
 
 sig.nbnd = 15;
 sig.ndiag_min = 8;
 sig.ndiag_max = 15;
-sig.coul_cutoff = 2; %coulomb truncation radius in sigma
+sig.coul_cutoff = 200; %coulomb truncation radius in sigma
 sig.no_symmetries_q_grid = 0;
 sig.exact_static_ch = 1;
 sig = sigma(eps, sig, sys, options, syms);
