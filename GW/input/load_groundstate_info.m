0a63a3d69a2ddf7b2576153e34c25d7923ece5a9
Zhengbang <zzb_99@hotmail.con>
2025-10-21 23:51:08 +0800

    add readqe feature

--- GW/input/load_groundstate_info.m
+++ GW/input/load_groundstate_info.m
@@ -49,6 +49,38 @@ end
 function data = load_qe_groundstate(dirin)
 % load_qe_groundstate - Load groundstate data from a QE pw.x output.
 % User must have previously run a QE pw.x calculation.
-error('load_qe_groundstate not implemented yet.');
+% error('load_qe_groundstate not implemented yet.');
+% function data = load_qe_groundstate(dirin)
+% load_qe_groundstate - Load groundstate data from a QE pw.x output.
+% User must have previously run a QE pw.x calculation.
+myneed = load_qe_from_folder(dirin);
+% error('load_qe_groundstate not implemented yet.');
+data = struct();
+data.rhor = myneed.rhor;
+data.Vxc = myneed.vxc;
+data.ev = myneed.ev;
+data.psig = myneed.psig;
+data.sys = myneed.sys;
+data.occupation = myneed.occupation;
+reciprocal_grid_info = struct();
+reciprocal_grid_info.fftgrid = [myneed.n1, myneed.n2, myneed.n3];
+reciprocal_grid_info.vol = myneed.vol;
+reciprocal_grid_info.idxnz = myneed.idxnz;
+reciprocal_grid_info.wfncut = myneed.wfncut;
+reciprocal_grid_info.xyz = myneed.mill;
+fprintf("Later, check units of wfncut!\n");
+
+
+data.reciprocal_grid_info = reciprocal_grid_info;
+data.nkibz = myneed.nkibz;
+data.kibz = myneed.kibz;
+data.kweight = myneed.kweight;
+data.nspin = myneed.nspin;
+data.nspinor = myneed.nspinor;
+
+symm = myneed.syms;
+data.syms = symm;
+
+
 end
 
