4011cc7e802444fea3b3a91966180ae7be6f096f
zhengbang <zzb_99@hotmail.com>
2026-07-31 12:52:56 +0800

    changes

--- src_profile/mapping/setmapping.m
+++
@@ -1,7 +0,0 @@
-function mapping = setmapping(GWinfor)
-  mapping =[];
-  mapping = setGomap(mapping, GWinfor);
-  mapping = setsymmGmap(mapping, GWinfor);
-  mapping = setRgridmap(mapping, GWinfor);
-
-end % EOF
