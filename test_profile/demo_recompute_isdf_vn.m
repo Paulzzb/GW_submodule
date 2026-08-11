29c16ec33759dff96201d7c84ab573b90aa2e1a8
zhengbang <zzb_99@hotmail.com>
2026-08-11 22:24:43 +0800

    Remove useless files

--- test_profile/demo_recompute_isdf_vn.m
+++
@@ -1,4 +0,0 @@
-function id_new = demo_recompute_isdf_vn(case_dir, output_dir, inputfile)
-%DEMO_RECOMPUTE_ISDF_VC  Wrapper: demo_recompute_isdf(..., 'vc', ...).
-  id_new = demo_recompute_isdf(case_dir, 'vn', output_dir, inputfile);
-end
